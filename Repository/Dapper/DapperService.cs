2f0cbdf7aa5f923891614e951f1a033b2117f3a3
João Engel <joaovi.engel@gmail.com>
2025-05-30 00:09:06 -0300

    Operacao Dapper
    Criando classe para operações no banco, para simplificar o uso do dapper dentro dos repositories

---
+++ Repository/Dapper/DapperService.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EasyAutoPartsHub.Repository.Dapper
+{
+    public interface IDapperService
+    {
+        Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, string? nomeTabela = null);
+        Task<List<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+        Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+    }
+
+    public class DapperService : IDapperService
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperService(IConfiguration configuration)
+        {
+            _conn = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public async Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, string? nomeTabela = null)
+        {
+            try
+            {
+                var result = await _conn.ExecuteScalarAsync<int>(sql, param, transaction, commandTimeout, commandType);
+
+                return result;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+
+        public async Task<List<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            try
+            {
+                var result = await _conn.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
+                return result.ToList();
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+
+        public async Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            try
+            {
+                return await _conn.QueryFirstOrDefaultAsync<T>(sql, param, transaction, commandTimeout, commandType);
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+    }
+}
