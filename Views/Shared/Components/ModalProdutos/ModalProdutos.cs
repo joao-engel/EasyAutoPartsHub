ce7fbc4d6b6861e67f126a3e6952481ed19451bb
João Engel <joaovi.engel@gmail.com>
2025-06-01 23:24:59 -0300

    Modal Produtos
    Criado modal para selecionar os produtos, podendo filtrar por texto e grupo

---
+++ Views/Shared/Components/ModalProdutos/ModalProdutos.cs
@@ -0,0 +1,34 @@
+﻿using EasyAutoPartsHub.Models;
+using EasyAutoPartsHub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyAutoPartsHub.Views.Shared.Components.ModalClientes
+{
+    public class ModalProdutos : ViewComponent
+    {
+        private readonly IProdutoServices _se;
+        private readonly IGrupoServices _seGrupo;
+
+        public ModalProdutos(IProdutoServices se, IGrupoServices seGrupo)
+        {
+            _se = se;
+            _seGrupo = seGrupo;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            try
+            {
+                List<ProdutoModel> lista = await _se.Listar(new ProdutoRQModel { Ativo = true });                
+                List<GrupoProdutoModel> lstGrupos = await _seGrupo.Listar(new GrupoProdutoModel {  });
+
+                ViewBag.Grupos = lstGrupos;
+                return View("Default", lista);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
