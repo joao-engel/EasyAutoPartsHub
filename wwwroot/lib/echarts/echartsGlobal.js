5f36136aceb75cee860dc8ecf287c8962bbedd41
João Engel <joaovi.engel@gmail.com>
2025-06-13 16:06:00 -0300

    Importacao Echarts
    Importado plugin utilização para geração de gráficos

---
+++ wwwroot/lib/echarts/echartsGlobal.js
@@ -0,0 +1,32 @@
+﻿const appGlobal = {
+    formatterValueIndex: function (value, index) {
+        return value.toLocaleString('pt-br', {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        });
+    },
+    formatterValor: function (valor) {
+        return valor.value.toLocaleString('pt-br', {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        });
+    },
+    color: 'black',
+    fontWeight: 'normal',
+    textBorderColor: 'black',
+    textBorderWidth: 0,
+    // splitLine linhas no meio do grafico
+    // https://echarts.apache.org/en/option.html#yAxis.splitLine.lineStyle.color
+    splitLine: {
+        lineStyle: {
+            color: "#000"
+        }
+    },
+    // axisLine texto abaixo do grafico [Seg, Ter, ....]
+    // https://echarts.apache.org/en/option.html#xAxis.axisLine.lineStyle.color
+    axisLine: {
+        lineStyle: {
+            color: "#000"
+        }
+    }
+};
