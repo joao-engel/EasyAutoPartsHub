17b46a98ada38175ce99be9a23562f64f733a7c8
João Engel <joaovi.engel@gmail.com>
2025-06-22 22:04:15 -0300

    Tela de Relatórios
    Criado tela com links que direcionam aos relatórios

---
+++ Controllers/RelatorioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyAutoPartsHub.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
