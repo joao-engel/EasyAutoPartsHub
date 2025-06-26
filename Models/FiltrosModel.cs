8036febb5c6461cca77dc0bdb34920751991f9f3
João Engel <joaovi.engel@gmail.com>
2025-06-25 22:37:29 -0300

    Header Titulo/Voltar
    Criado cabeçalho com o título da página e botão de voltar.
    Será usado em todos relatórios

---
+++ Models/FiltrosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyAutoPartsHub.Models;
+
+public class FiltrosModel
+{
+    public string Titulo { get; set; }
+    public string ControllerVoltar { get; set; }
+    public string ActionVoltar { get; set; }
+    public Dictionary<string, string> RotasVoltar { get; set; }
+}
---
+++ Views/Shared/Components/FiltroHead/FiltroHead.cs
@@ -0,0 +1,13 @@
+﻿using EasyAutoPartsHub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyAutoPartsHub.Views.Shared.Components.FiltroHead
+{
+    public class FiltroHead : ViewComponent
+    {
+        public IViewComponentResult Invoke(FiltrosModel model)
+        {
+            return View("Default", model);
+        }
+    }
+}
